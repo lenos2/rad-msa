ada6b72002106a79fc287e79069ab27faa6863e8
Mac Me <nickdacrack@gmail.com>
2017-09-27 17:20:35 +0200

    Added a few new features to the app

---
+++ app/src/main/java/com/kapture/mystreezofafrica/pojos/User.java
@@ -0,0 +1,45 @@
+
+package com.kapture.mystreezofafrica.pojos;
+
+
+@SuppressWarnings("unused")
+public class User {
+
+    private String mDob;
+    private String mGender;
+    private String mName;
+    private String mProfileUrl;
+
+    public String getDob() {
+        return mDob;
+    }
+
+    public void setDob(String dob) {
+        mDob = dob;
+    }
+
+    public String getGender() {
+        return mGender;
+    }
+
+    public void setGender(String gender) {
+        mGender = gender;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+
+    public String getProfileUrl() {
+        return mProfileUrl;
+    }
+
+    public void setProfileUrl(String profileUrl) {
+        mProfileUrl = profileUrl;
+    }
+
+}
